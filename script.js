2829b0f25d5ca8b8e7155688ea25c3e49d642c42
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-09-13 20:42:20 +0530

    using array to make nested child

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ const heading = React.createElement(
   "hello world from react"
 );
 
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am Chidren Element or nested"),
+    React.createElement("h2", {}, "I am Sibling to the first H1"),
+  ])
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
- 
+root.render(parent);
