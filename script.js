a115b3b7739dde00cf0b51adacc3437b13a753c9
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-09-13 20:31:53 +0530

    giving id and styling

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const heading=React.createElement("h1",{},"hello world from react");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello world from react"
+);
 
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+ 
