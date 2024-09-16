7e9ce3f4b8296fa112dd4931599faee2db1eac07
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-09-17 01:06:18 +0530

    more with jsx

--- script.js
+++ script.js
@@ -9,6 +9,42 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //its hard to write this code
 //jsx=>created=>merge html and js=>not html in js=>html like syntax
-const jsxheading = <h1 id="heading">Hello jee</h1>;
+//when giving atribute to jsx use camel casing
 
-root.render(jsxheading);
+const jsxheading = <h1 className="heading">Hello jee</h1>;
+//babel transpiles jsx
+//babel is converting this code to html
+
+// root.render(jsxheading);
+//react component=>two types of components:
+//class bassed component=>old way
+//functional component=>new way =>mostly used
+
+
+//functional component=>normal js function
+const HeadingComponent=()=>{
+    return(
+        <div className="container">
+            <h1>Hello from functional Component:Heading</h1>
+        </div>
+    )
+}
+//element in jsx
+const element=(<h1>hello I am element</h1>);
+
+//component composition
+const TitleComponent=()=>{
+    return(
+        <div className="container">
+            <HeadingComponent/> 
+            {element}
+            <h1>Hello from functional Component:Title</h1>
+        </div>
+    )
+}
+
+//we can use js in jsx using {}=> this brackets
+
+
+//need to render functional component as element
+root.render(<TitleComponent/>);
