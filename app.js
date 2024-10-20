5c18d533107673adcbdeee9a27050bac5191e7ad
Ankush1oo8 <ankushchudiwalwit@gmail.com>
2024-10-21 00:54:57 +0530

    Lecture3

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = React.createElement("h1", {}, "Hello world from REACT");
+//React elements
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello from react"
+);
+
+const Heading1 = () => <h1 id="heading"> hello there from jsx</h1>;
+
+const JsxHeading = () => {
+  return (
+    <>
+      <h1>Hello there</h1>
+      <h2>I am Ankush</h2>
+    </>
+  );
+};
+
+let n = "Ankush";
+//components
+const HeadingComponents = () => {
+  return (
+    <div>
+      <h1>hello {n}</h1>
+      <Heading1 />
+      <JsxHeading />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponents />);
